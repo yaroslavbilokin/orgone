33aecbea746693d94de3c9e1089373ee445f7be6
Yaroslav Bilokin <yaroslavbilokin41@gmail.com>
2022-11-09 12:39:59 +0200

    Add ORGONE project structure

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    trailingComma: "all",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 100,
+};
